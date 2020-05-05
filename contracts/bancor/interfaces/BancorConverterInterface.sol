bb0e034ace5d7dff6911cc674d190f50fae06f15
Ruslan <ruslan777mirov@gmail.com>
2020-05-05 17:11:23 +0300

    Update BancorConverterInterface with connectoTokens (bug can be here)

--- contracts/bancor/interfaces/BancorConverterInterface.sol
+++ contracts/bancor/interfaces/BancorConverterInterface.sol
@@ -1,7 +1,7 @@
 import "../../zeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
 interface BancorConverterInterface {
-  IERC20[] public connectorTokens;
+  function connectorTokens(uint index) external view returns(IERC20);
   function fund(uint256 _amount) external;
   function liquidate(uint256 _amount) external;
   function getConnectorBalance(IERC20 _connectorToken) external view returns (uint256);
